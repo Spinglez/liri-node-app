c6e234bae7f260f3d17290b2216758bfafe28789
Spinglez <42229372+Spinglez@users.noreply.github.com>
2018-11-26 13:18:57 -0800

    added switch statements for action arguments, changed spotify call to a function for use with switch statements on action

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsIn = {
+  key: process.env.BANDSIN_KEY
+}
+
+exports.omdb = {
+  key: process.env.OMDB_KEY
+}
--- liri.js
+++ liri.js
@@ -6,11 +6,6 @@ const Spotify = require('node-spotify-api');
 
 const fs = require('fs');
 
-// const keys = {
-//   bandsIn: '',
-//   omdb: '9199b32b',
-// }
-
 const dotenv = require('dotenv');
 
 const keys = dotenv.config()
@@ -19,30 +14,64 @@ if (keys.error) {
   throw result.error
 }
 
-console.log(keys.parsed.SPOTIFY_ID);
+// console.log(keys.parsed.SPOTIFY_ID);
 
-const arg = process.argv.slice(2).join(" ");
+const action = process.argv[2];
+const arg = process.argv.slice(3).join(" ");
 
 const spotify = new Spotify({
   id: keys.parsed.SPOTIFY_ID,
   secret: keys.parsed.SPOTIFY_SECRET
 });
 
-spotify
-  .search({type: "track", query: arg, limit:5}, function(err, data){
-    if (err) {
-        return console.error(err);
+switch (action) {
+case "spotify-this-song":
+  spot();
+  break;
+
+case "concert-this":
+  conc();
+  break;
+
+case "movie-this":
+  mov();
+  break;
+
+case "do-what-it-says":
+  doWhat();
+  break;
+}
+
+console.log(arg);
+
+function spot(){
+  if (arg == '') {
+    spotify.search({type: "track", query: 'Ace of Base The Sign', limit:1}, function(err, data){
+      if (err) {
+          return console.error(err);
+      }
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        console.log('Artist: ' + JSON.stringify(data.tracks.items[i].artists[0].name, null, 2));
+        console.log('Album: ' + JSON.stringify(data.tracks.items[i].album.name, null, 2));
+        console.log('Track Name: ' + JSON.stringify(data.tracks.items[i].name, null, 2));
+        console.log('Preview: ' + JSON.stringify(data.tracks.items[i].preview_url, null, 2) + '\n');
+      }
+    });
+  }else {
+      spotify.search({type: "track", query: arg, limit:5}, function(err, data){
+        if (err) {
+            return console.error(err);
+        }
+        // console.log(JSON.stringify(data.tracks.items[0], null, 2));
+        for (var i = 0; i < data.tracks.items.length; i++) {
+          console.log('Artist: ' + JSON.stringify(data.tracks.items[i].artists[0].name, null, 2));
+          console.log('Album: ' + JSON.stringify(data.tracks.items[i].album.name, null, 2));
+          console.log('Track Name: ' + JSON.stringify(data.tracks.items[i].name, null, 2));
+          console.log('Preview: ' + JSON.stringify(data.tracks.items[i].preview_url, null, 2) + '\n');
+        }
+      });
     }
-    console.log(JSON.stringify(data.tracks.items[0], null, 2));
-    for (var i = 0; i < data.tracks.items.length; i++) {
-      console.log('Artist: ' + JSON.stringify(data.tracks.items[i].artists[0].name, null, 2));
-      console.log('Track Name: ' + JSON.stringify(data.tracks.items[i].name, null, 2));
-      console.log('Preview: ' + JSON.stringify(data.tracks.items[i].preview_url, null, 2) + '\n');
-    }
-    for(let prop in data.tracks.items) {
-        console.log(JSON.stringify(prop));
-    }
-  });
+  }
 
 // axios.get(spot + 'creed' + keys.spotify).then(
 //   function(res){
